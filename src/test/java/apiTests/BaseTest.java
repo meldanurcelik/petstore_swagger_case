5e1dedac8df91b15c386740d54464bbe758268a7
meldanurcelik <melda_celik@hotmail.com>
2025-02-06 13:03:26 +0300

    CreatePetTest için pozitif ve negatif senaryolar eklendi

--- src/test/java/apiTests/BaseTest.java
+++ src/test/java/apiTests/BaseTest.java
@@ -1,13 +1,26 @@
 package apiTests;
 
 import io.restassured.RestAssured;
-import org.testng.annotations.BeforeClass;
+import io.restassured.http.*;
+import io.restassured.response.Response;
+import org.testng.annotations.*;
+
+import static io.restassured.RestAssured.given;
 
 public class BaseTest {
 
     @BeforeClass
     public void setup() {
         RestAssured.baseURI = "https://petstore.swagger.io/v2";
+    }
 
+    public Response sendPostRequest(String requestBody) {
+        return given()
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .when()
+                .post("/pet")
+                .then()
+                .extract().response();
     }
 }
--- src/test/java/apiTests/CreatePetTest.java
+++ src/test/java/apiTests/CreatePetTest.java
@@ -1,11 +1,8 @@
 package apiTests;
 
-import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.*;
-
-import static io.restassured.RestAssured.*;
 
 public class CreatePetTest extends BaseTest { // BaseTest'ten extends edildi
 
@@ -20,16 +17,105 @@ public class CreatePetTest extends BaseTest { // BaseTest'ten extends edildi
                 "  \"status\": \"available\"\n" +
                 "}";
 
-        Response response = given()
-                .contentType(ContentType.JSON)
-                .body(requestBody)
-                .when()
-                .post("/pet")
-                .then()
-                .extract().response();
+        Response response = sendPostRequest(requestBody);
 
         Assert.assertEquals(response.getStatusCode(), 200, "Status code is not 200!");
         Assert.assertEquals(response.jsonPath().getString("name"), "Buddy", "Pet name does not match!");
-
     }
+
+    @Test
+    public void testCreatePetWithMissingName() {
+        String requestBody = "{\n" +
+                "  \"id\": 0,\n" +
+                "  \"category\": { \"id\": 0, \"name\": \"string\" },\n" +
+                "  \"name\": \"\",\n" +
+                "  \"photoUrls\": [\"string\"],\n" +
+                "  \"tags\": [{ \"id\": 0, \"name\": \"string\" }],\n" +
+                "  \"status\": \"available\"\n" +
+                "}";
+
+        Response response = sendPostRequest(requestBody);
+
+        Assert.assertEquals(response.getStatusCode(), 200, "Status code is not 200!");
+    }
+
+    @Test
+    public void testCreatePetWithMissingStatus() {
+        String requestBody = "{\n" +
+                "  \"id\": 0,\n" +
+                "  \"category\": { \"id\": 0, \"name\": \"string\" },\n" +
+                "  \"name\": \"Pet1\",\n" +
+                "  \"photoUrls\": [\"url1\"],\n" +
+                "  \"tags\": [{ \"id\": 0, \"name\": \"tag1\" }],\n" +
+                "  \"status\": \"\"\n" +
+                "}";
+
+        Response response = sendPostRequest(requestBody);
+
+        Assert.assertEquals(response.getStatusCode(), 200, "Status code is not 200!");
+    }
+
+    @Test
+    public void testCreatePetWithInvalidPhotoUrls() {
+        String requestBody = "{\n" +
+                "  \"id\": 0,\n" +
+                "  \"category\": { \"id\": 0, \"name\": \"string\" },\n" +
+                "  \"name\": \"Pet1\",\n" +
+                "  \"photoUrls\": [],\n" +
+                "  \"tags\": [{ \"id\": 0, \"name\": \"tag1\" }],\n" +
+                "  \"status\": \"available\"\n" +
+                "}";
+
+        Response response = sendPostRequest(requestBody);
+
+        Assert.assertEquals(response.getStatusCode(), 200, "Status code is not 200!");
+    }
+
+    @Test
+    public void testCreatePetWithInvalidId() {
+        String requestBody = "{\n" +
+                "  \"id\": -1,  // Geçersiz id\n" +
+                "  \"category\": { \"id\": 0, \"name\": \"string\" },\n" +
+                "  \"name\": \"Pet1\",\n" +
+                "  \"photoUrls\": [\"url1\"],\n" +
+                "  \"tags\": [{ \"id\": 0, \"name\": \"tag1\" }],\n" +
+                "  \"status\": \"available\"\n" +
+                "}";
+
+        Response response = sendPostRequest(requestBody);
+
+        Assert.assertEquals(response.getStatusCode(), 400, "Status code is not 400!");
+    }
+
+    @Test
+    public void testCreatePetWithInvalidJsonFormat() {
+        String requestBody = "{\n" +
+                "  \"id\": 0,\n" +
+                "  \"category\": { \"id\": 0, \"name\": \"string\" },\n" +
+                "  \"name\": \"Pet1\",\n" +
+                "  \"photoUrls\": [\"url1\"],\n" +
+                "  \"tags\": [{ \"id\": 0, \"name\": \"tag1\" }],\n" +
+                "  \"status\": \"available\", \n" +
+                "}";
+
+        Response response = sendPostRequest(requestBody);
+
+        Assert.assertEquals(response.getStatusCode(), 400, "Status code is not 400!");
+    }
+
+    @Test
+    public void testCreatePetWithMissingCategory() {
+        String requestBody = "{\n" +
+                "  \"id\": 0,\n" +
+                "  \"name\": \"Pet1\",\n" +
+                "  \"photoUrls\": [\"url1\"],\n" +
+                "  \"tags\": [{ \"id\": 0, \"name\": \"tag1\" }],\n" +
+                "  \"status\": \"available\"  // Eksik category \n" +
+                "}";
+
+        Response response = sendPostRequest(requestBody);
+
+        Assert.assertEquals(response.getStatusCode(), 400, "Status code is not 400!");
+    }
+
 }
